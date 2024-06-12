10634db6b8f058c59238885c16c754b602993c5e
Thomas Winkler <tom.winkler@gmail.com>
2024-06-12 20:30:12 +0200

    Added GDBMI dump on breakpoint timeout.

--- dottmi/breakpoint.py
+++ dottmi/breakpoint.py
@@ -181,6 +181,7 @@ class HaltPoint(Breakpoint):
         try:
             self._q.get(block=True, timeout=timeout)
         except queue.Empty:
+            self._gdb_client.gdb_mi.debug_capture.dump()
             raise TimeoutError(f'Timeout ({timeout}s) while waiting to reach halt point at {self._location}.') from None
 
     def reached_internal(self, payload=None) -> None:
